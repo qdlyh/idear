88e6332d57e712c7b57ab344d89a0e0aa3d8ece0
qdlyh <liang620523@163.com>
2017-11-13 22:54:58 +0800

    Update cart.js

--- PCidear/src/js/shopping/cart.js
+++ PCidear/src/js/shopping/cart.js
@@ -15,16 +15,50 @@ $('.subtract-btn').click(function () {
     if (parseInt(goodsCount.val()) < 1) {
         goodsCount.val(1)
     }
-    if(goodsCount.val() ===''){
+    if (goodsCount.val() === '') {
         goodsCount.val(1)
     }
+    setTotal();
 })
 /* input数量加 */
 $('.add-btn').click(function () {
     var goodsCount = $(this).parents('.goods-count').find('.goods-input')
     //console.log(goodsCount)
     goodsCount.val(parseInt(goodsCount.val()) + 1)
-    if(goodsCount.val() ===''){
+    if (goodsCount.val() === '') {
         goodsCount.val(1)
     }
+    setTotal();
 })
+
+$('.delete-goods').click(function(){
+    $(this).parents('.cart-container').remove();
+      setTotal();
+})
+
+function setTotal() {
+    var s = 0;
+    $(".cart-money").each(function () {
+        //console.log($(this))
+         s += parseInt($(this).find('.goods-input').val())*
+             parseFloat($(this).find('.money-text').text());
+             console.log(s)
+    });
+   $("#total").html(s.toFixed(0));
+      goodsCount();
+}
+setTotal();
+
+
+
+
+function goodsCount() {
+    var s = 0;
+    $(".cart-money").each(function () {
+        //console.log($(this))
+         s += parseInt($(this).find('.goods-input').val())
+             //parseFloat($(this).find('.money-text').text());
+             console.log(s)
+    });
+   $("#count").html(s.toFixed(0));
+}
